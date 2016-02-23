6c05d80913e38e7cea3fae4bd71c7c3aa05cdfcb
Vladislav Pakhomov <d1skortneru@yandex.ru>
2016-02-24 00:35:29 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,8 @@
-sudo ﻿ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+#!/usr/bin/env bash
+
+sudo rm /etc/nginx/sites-enabled/default
+
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+
+gunicorn -b 0.0.0.0:8080 hello:app &
