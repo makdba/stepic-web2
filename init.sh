8dc14632019152a7dce12c6da6b0e2d9d80432e6
d1skort <d1skortneru@yandex.ru>
2016-03-03 11:36:04 +0000

    django!

--- init.sh
+++ init.sh
@@ -5,4 +5,6 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
-gunicorn -b 0.0.0.0:8080 hello:app &
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
+sudo /etc/init.d/gunicorn restart
