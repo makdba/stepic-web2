e01d78256721b66088723b99630194c33674be0e
Vladislav Pakhomov <d1skortneru@yandex.ru>
2016-03-11 15:51:57 +0900

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login/$', views.my_login, name='login'),
     url(r'^signup/$', views.signup, name='signup'),
-    url(r'^question/(?P<id>[^/]+)/$', views.question, name='question'),
+    url(r'^question/(?P<id>\d+)/$', views.question, name='question'),
     url(r'^ask/$', views.ask, name='ask'),
     url(r'^popular/$', views.popular, name='popular'),
-    url(r'^new/$', views.test, name='new'),
+    url(r'^new/$', views.index, name='new'),
     url(r'^answer/$', views.answer, name='answer'),
 ]
