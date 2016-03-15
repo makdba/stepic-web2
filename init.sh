7941a6291b5962e3ddda4876d2eaac1a38e34638
Mikhail Kozlov <mikhkozlov@yandex.ru>
2016-03-15 14:08:57 +0300

    init.sh

--- init.sh
+++ init.sh
@@ -7,6 +7,14 @@ sudo /etc/init.d/nginx restart
 
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo ln -s /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
-sudo /etc/init.d/gunicorn restart
+
 
 sudo /etc/init.d/mysql restart
+#MySQL initial actions
+mysql -uroot -e"CREATE DATABASE stepic"
+mysql -uroot -e"CREATE USER 'admin'@'localhost' IDENTIFIED BY 'qwerty'"
+mysql -uroot -e"GRANT ALL ON stepic.* TO 'admin'@'localhost'"
+
+sudo python /home/box/web/ask/manage.py syncdb
+
+sudo /etc/init.d/gunicorn restart
